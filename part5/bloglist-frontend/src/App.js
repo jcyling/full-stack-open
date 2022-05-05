d102b03d09184e9dd6fa29b567bbc4892ffbb867
jcyling <jane.cy.ling@gmail.com>
2022-05-05 15:13:30 +0800

    Add blogslist frontend initial commit

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      // Pass in login form parameters to POST request
+      const user = await loginService.login({
+        username, password,
+      })
+
+      // Set user if successful
+      setUser(user)
+      
+      // Empty form parameters
+      setUsername('')
+      setPassword('')
+
+    } catch (exception) {
+      console.log("Unsuccessful login")
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)} />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+
+  return (
+    <div>
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in</p>
+        </div>
+      }
+      <div>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
